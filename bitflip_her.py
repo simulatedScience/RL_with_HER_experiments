7fc33ad1d7daf41f3d3d7f495a603147d9f3894e
simulatedScience <62523381+simulatedScience@users.noreply.github.com>
2022-11-09 10:46:12 +0000

    added module docstring

--- bitflip_her.py
+++ bitflip_her.py
@@ -1,3 +1,10 @@
+"""
+This module implements a reinforcement learning algorithm to solve the bitflip environment:
+
+1. generate two random tuples of length n with values 0 or 1.
+2. one tuple is the initial state, the other the goal.
+3. There are n actions, each flipping a single bit in the tuple.
+"""
 import numpy as np
 import tensorflow.keras as keras
 
@@ -6,12 +13,21 @@ def gen_bitflip_start(n: int=20) -> np.ndarray:
 
 def train_model(
   neural_net: keras.Model,
-  max_epochs: int,
+  max_episodes: int,
   learning_rate: float,
   exploration_rate: float):
-  play_epoch()
-
-def play_epoch(max_epoch_length: int):
   """
-  play a single epoch
+  train model with given parameters for a given number of episodes
+
+  Args:
+      neural_net (keras.Model): _description_
+      max_episodes (int): _description_
+      learning_rate (float): _description_
+      exploration_rate (float): _description_
+  """
+  play_episode()
+
+def play_episode(max_episode_length: int):
+  """
+  play a single episode
   """
